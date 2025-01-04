1c98b9ceb3d5bbc692e5934de5a1f5d896038f4b
Tim Sauer <Mail@tim-Sauer.com>
2025-01-05 00:01:35 +0100

    Add rotating proxies to prevent the site from blocking the crawler

--- webscraper.py
+++ webscraper.py
@@ -3,6 +3,39 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import pandas as pd
 import time
+from lxml.html import fromstring
+from itertools import cycle
+import traceback
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
+
+def validate_response(response):
+    if response.status_code != 200:
+        print("Failed to fetch data")
+        print(response.status_code)
+        return False
+    return True
+
+def get_data(url, proxy):
+    try:
+        response = requests.get(url, proxies={"http": proxy, "https": proxy})
+        print("Fetched data")
+        return response
+    except:
+        print("Skipping. Connection error")
+        return get_data(url, next(proxy_pool))
+
+proxies = get_proxies()
+proxy_pool = cycle(proxies)
 
 standings_url = 'https://fbref.com/en/comps/9/Premier-League-Stats'
 
@@ -10,9 +43,15 @@ years = list(range(2024, 2020, -1))
 all_matches = []
 
 for year in years:
-    data = requests.get(standings_url)
+    #Request No.1
+    #Get the standings table
+    proxie = next(proxy_pool)
+    data = get_data(standings_url, proxie)
+    if not validate_response(data):
+        continue
+    time.sleep(1)
     soup = BeautifulSoup(data.text)
-    standings_table = soup.select('table.stats_table')
+    standings_table = soup.select('table.stats_table')[0]
 
     links = standings_table.find_all('a')
     links = [l.get('href') for l in links]
@@ -25,21 +64,37 @@ for year in years:
 
     for team_url in team_urls:
         team_name = team_url.split('/')[-1].replace('-Stats', ' ').replace('-', ' ')
-        data = requests.get(team_url)
+
+        #Request No.2
+        #Get the matches table
+        proxie = next(proxy_pool)
+        data = get_data(team_url, proxie)
+        if not validate_response(data):
+            continue
+        time.sleep(1)
         matches = pd.read_html(data.text, match="Scores & Fixtures")
         soup = BeautifulSoup(data.text)
         links = soup.find_all('a')
         links = [l.get('href') for l in links]
         links = [l for l in links if l and '/all_comps/shooting' in l]
 
-        data = requests.get(urljoin("https://fbref.com", links[0]))
-        shooting = pd.read_html(data.text, match="Shooting")[0]
-        shooting.columns = shooting.columns.droplevel()
-
+        #Request No.3
+        #Get the shooting table
+        proxie = next(proxy_pool)
+        data = get_data(urljoin("https://fbref.com", links[0]), proxie)
+        if not validate_response(data):
+            continue
+        time.sleep(1)
+        try:
+            shooting = pd.read_html(data.text, match="Shooting")[0]
+            shooting.columns = shooting.columns.droplevel()
+        except ValueError:
+            continue
         try:
             team_data = matches[0].merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")
         except ValueError:
-            continue    
+            continue
+
         team_data = team_data[team_data["Comp"] == "Premier League"]
         team_data["Season"] = year
         team_data["Team"] = team_name
